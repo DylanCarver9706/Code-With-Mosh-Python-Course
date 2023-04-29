17c26e8085da098d721f7e7bbb555ee8735c5ebd
Dylan Carver <dylancarver14@gmail.com>
2023-04-29 14:53:39 -0500

    define and print variables in terminal

---
+++ HelloWorld/Variables.py
@@ -0,0 +1,15 @@
+price = 10 # integer variable gets stored in memory/ram
+price = 20 # Python gets run line by line so it updates
+rating = 4.9 # Float variable
+name = 'Dylan' # string variable
+is_published = True # boolean variable
+print(name)
+
+# Exercise: We check in a patient named John Smith.
+            # He's 20 years old and is a new patient.
+# Write 3 variables for this patient:
+name = 'John Smith'
+age = 20
+is_new_patient = True
+visitor_type = 'patient'
+gender = 'male'
