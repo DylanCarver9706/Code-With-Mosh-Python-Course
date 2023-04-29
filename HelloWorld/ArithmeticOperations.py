141cb18d0522ae0ffa8775ef7b42ded7bd7bc5b4
Dylan Carver <dylancarver14@gmail.com>
2023-04-29 17:20:17 -0500

    Learned Python arithmetic operators

---
+++ HelloWorld/ArithmeticOperations.py
@@ -0,0 +1,20 @@
+print(10 / 3) # + - * / // % **
+# 3.33333333
+
+print(10 // 3)
+# 3
+
+x = 10
+x = x + 3
+print(x)
+# 13
+
+x = 10
+x += 3 #augmented assignment operator
+print(x)
+# 13
+
+x = 10
+x -= 3
+print(x)
+# 7
