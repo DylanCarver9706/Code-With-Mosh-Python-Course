7474e0e0e5a58fcb923f52f31345b4e25ec1cf01
Dylan Carver <dylancarver14@gmail.com>
2023-04-30 18:28:35 -0500

    Learned about unpacking a tuple

---
+++ HelloWorld/Unpacking.py
@@ -0,0 +1,16 @@
+coordinates = (1, 2, 3) # Tuple
+
+# x = coordinates[0]
+# y = coordinates[1]
+# z = coordinates[2]
+
+# Since a Tuple cannot be altered, we need a way
+# to use the data.
+
+# This is the same as the variable assignment above
+x, y, z = coordinates # Unpacking
+
+print(x)
+print(y)
+print(z)
+
