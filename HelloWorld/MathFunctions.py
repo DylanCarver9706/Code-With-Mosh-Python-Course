1fc174c2b9e25b6684dac400e1cd66944546c7ec
Dylan Carver <dylancarver14@gmail.com>
2023-04-29 20:31:43 -0500

    Learn math module + methods

---
+++ HelloWorld/MathFunctions.py
@@ -0,0 +1,17 @@
+import math
+
+x = 2.9
+print(round(x))
+# 3
+
+print(abs(-2.9)) # absolute value / always positive
+# 2.9
+
+print(math.ceil(2.9)) # external module. needs to be imported at top
+# 3
+
+print(math.floor(2.9))
+# 2
+
+# Go to https://docs.python.org/3/library/math.html
+# for an explaination on all math methods
