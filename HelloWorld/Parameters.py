8d9f61f7b35ce720554860a0e4ec5c1a47d847b1
Dylan Carver <dylancarver14@gmail.com>
2023-05-03 16:30:39 -0500

    Learned function parameters

---
+++ HelloWorld/Parameters.py
@@ -0,0 +1,10 @@
+def greet_user(first_name, last_name): # function get defined all lowercase and with an underscore as a space
+    # Parameter is same as name = John inside function
+    print(f'Hi {first_name} {last_name}!')
+    print('Welcome aboard')
+# Add 2 line breaks after a function
+
+
+print("Start")
+greet_user("John", "Smith")
+print("Finish")
