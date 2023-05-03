3611b0303c30bd9e7eddb55005984b2aafdf156d
Dylan Carver <dylancarver14@gmail.com>
2023-05-03 16:24:35 -0500

    Learned functions

---
+++ HelloWorld/Functions.py
@@ -0,0 +1,9 @@
+def greet_user(): # function get defined all lowercase and with an underscore as a space
+    print('Hi there!')
+    print('Welcome aboard')
+# Add 2 line breaks after a function
+
+
+print("Start")
+greet_user()
+print("Finish")
