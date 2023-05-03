1f9d10a43d982b00b6a4435fdf896b70e793467c
Dylan Carver <dylancarver14@gmail.com>
2023-05-03 16:46:39 -0500

    Learned the importance of return statements in functions

---
+++ HelloWorld/ReturnStatement.py
@@ -0,0 +1,9 @@
+def square(number):
+    # print(number * number)
+    #if no return statement is given in a function, then 'None' will return
+    return number * number
+
+result = square(3)
+print(result)
+# or
+print(square(3))
